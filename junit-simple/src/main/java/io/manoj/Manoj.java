f78f24e0372204c23fd265c631c959c80452c75d
Mahendra Chakravarthi <manojmunna70@gmail.com>
2022-01-28 16:04:39 +0530

    juint-simple

---
+++ junit-simple/src/main/java/io/manoj/Manoj.java
@@ -0,0 +1,30 @@
+package io.manoj;
+
+public class Manoj {
+	
+	public int add(int a,int b) {
+		return a+b;
+	}
+
+	public int multiply(int a,int b) {
+		return a*b;
+	}
+	
+
+	public int sub(int a,int b) {
+		return a-b;
+	}
+	
+
+	public int divide(int a,int b) {
+		return a/b;
+	}
+	
+	
+	public double computerCircleArea(double radius)
+	{
+		return Math.PI*radius *radius;
+		
+	}
+	
+}
